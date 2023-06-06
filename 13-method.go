51835d6199e1da2ccf50e871c2225ec6430683cb
Milad Rasouli <Milad75Rasouli@gmail.com>
2023-06-07 01:19:36 +0400

    add none-struct class

--- 13-method.go
+++ 13-method.go
@@ -22,10 +22,22 @@ func (vehicle Vehicle) Drive2(name string) (result string) {
 	return
 }
 
+// none-struct
+type MyFloat float32
+
+func (f MyFloat) Squer() float32 {
+	result := float32(f * f)
+	return result
+}
+
 func main() {
 	var car1 Vehicle = Vehicle{Color: "Blue", Speed: 220}
 	result := car1.Drive("BMW")
 	fmt.Println(result)
 	result = car1.Drive2("Benz")
 	fmt.Println(result)
+
+	f := MyFloat(3.14)
+	fmt.Printf("%v\n", f.Squer())
+
 }
