3dc5185d1ffe03310f11076da06b8eba339a516c
MiladRasouli <Milad75Rasouli@gmail.com>
2023-06-09 16:25:29 +0430

    add interface 2

---
+++ 17-interface2.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+type I interface {
+	M()
+}
+
+type T struct {
+	S string
+}
+
+// This method means type T implements the interface I,
+// but we don't need to explicitly declare that it does so.
+func (t T) M() {
+	fmt.Println(t.S)
+}
+
+type MM struct {
+	f float64
+}
+
+func (m *MM) M() {
+	fmt.Println(m.f)
+}
+
+type Do struct {
+	Q I
+}
+
+func (d *Do) SetQ(i I) {
+	d.Q = i
+}
+func (d *Do) DoIt() {
+	d.Q.M()
+}
+func main() {
+	var i I
+	i = T{"hello"}
+	i.M()
+	i = &MM{23.44}
+	i.M()
+}
