8d15a5301085cb49f56cb3030629047d87c867b8
MiladRasouli <Milad75Rasouli@gmail.com>
2023-06-23 16:03:38 +0330

    add http post and get

--- 32-http.go
+++ 32-http.go
@@ -1,23 +1,56 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 )
 
+type PostData struct {
+	Name   string `json:Name`
+	Action string `json:Action`
+	Count  int32  `json:Count`
+}
+
 func main() {
-	url := "https://echo.hoppscotch.io/text=Hi_Bunny"
+	baseURL := "https://webhook.site/481692e5-6b56-45f7-b165-b5ca77cfbd19"
 
-	responce, err := http.Get(url)
+	params := url.Values{}
+	params.Set("ID", "3299")
+	params.Set("Name", "Josh")
+
+	get_url := fmt.Sprintf("%s?%s", baseURL, params.Encode())
+	// fmt.Println(get_url)
+	res, err := http.Get(get_url)
 	if err != nil {
-		// fmt.Errorf("ERROR:%s\n", err.Error())
-		// return
-		// we can use Fatalf instead
-		log.Fatalf("ERROR:%s\n", err.Error())
+		log.Fatalf("Get ERROR:%s\n", err.Error())
 	}
-	defer responce.Body.Close()
 
-	io.Copy(os.Stdout, responce.Body)
+	defer res.Body.Close()
+	io.Copy(os.Stdout, res.Body)
+	fmt.Println("======================")
+	user_post_data := &PostData{
+		Name:   "Tom",
+		Action: "studing books",
+		Count:  3,
+	}
+
+	var result_post_data bytes.Buffer
+	enc := json.NewEncoder(&result_post_data)
+	if err := enc.Encode(user_post_data); err != nil {
+		log.Fatal("ERROR: %s\n", err.Error())
+	}
+	// fmt.Printf("%s\n", result_post_data)
+	post_response, err := http.Post(baseURL, "application/json", &result_post_data)
+	if err != nil {
+		log.Fatalf("ERROR:%s\n", err)
+	}
+	defer post_response.Body.Close()
+	io.Copy(os.Stdout, post_response.Body)
+
 }
